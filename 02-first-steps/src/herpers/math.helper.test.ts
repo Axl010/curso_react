588b11c9b641a6c5b8ebcba5ba55f49412071f24
Axl <actcelchirinos1@gmail.com>
2026-03-16 21:39:21 -0400

    feat: implement coverage

--- 02-first-steps/src/herpers/math.helper.test.ts
+++ 02-first-steps/src/herpers/math.helper.test.ts
@@ -1,7 +1,7 @@
 import { expect, test, describe } from "vitest";
-import { add } from "./math.helper";
+import { add, divide, multiply, subtract } from "./math.helper";
 
-describe("subtract", () => {
+describe("add", () => {
     test('shoul add two positives numbers', () => {
 
         // ! 1. Arrage
@@ -14,4 +14,48 @@ describe("subtract", () => {
         // 3. Assert
         expect(result).toBe(a + b);
     })
-})
+});
+
+describe("subtract", () => {
+    test('shoul add two positives numbers', () => {
+
+        // ! 1. Arrage
+        const a = 1;
+        const b = 3;
+    
+        // ! 2. Act
+        const result = subtract(a, b);
+    
+        // 3. Assert
+        expect(result).toBe(a - b);
+    })
+});
+
+describe("multiply", () => {
+    test('shoul multiplay two negative numbers', () => {
+
+        // ! 1. Arrage
+        const a = -2;
+        const b = -4;
+    
+        // ! 2. Act
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    });
+});
+
+describe("subtract", () => {
+    test('shoul divide two positives numbers', () => {
+
+        // ! 1. Arrage
+        const a = 2;
+        const b = 4;
+    
+        // ! 2. Act
+        const result = divide(a, b);
+    
+        // 3. Assert
+        expect(result).toBe(a / b);
+    })
+});
